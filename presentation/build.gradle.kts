04e98a60a36600b67dd6696c392b7ddefa74b4c7
kim dong kyung <kdkd422@naver.com>
2025-06-06 11:03:36 +0900

    setting - kotlinx.immutable 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -56,6 +56,9 @@ dependencies {
     implementation(libs.orbit.viewmodel)
     implementation(libs.orbit.compose)
 
+    //immutable
+    implementation(libs.kotlinx.immutable)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
