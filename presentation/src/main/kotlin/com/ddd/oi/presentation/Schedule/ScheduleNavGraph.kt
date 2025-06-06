2b9502ab5ea3e169972a56f97ae1b7a480272ead
kim dong kyung <kdkd422@naver.com>
2025-06-06 11:04:07 +0900

    Feat - Schedule NavGraph

---
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/Schedule/ScheduleNavGraph.kt
@@ -0,0 +1,15 @@
+package com.ddd.oi.presentation.Schedule
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.ddd.oi.presentation.core.navigation.MainTabRoute
+
+fun NavController.navigateToSchedule(navOptions: NavOptions) {
+    navigate(MainTabRoute.Schedule, navOptions)
+}
+
+fun NavGraphBuilder.scheduleNavGraph() {
+    composable<MainTabRoute.Schedule> {  }
+}
