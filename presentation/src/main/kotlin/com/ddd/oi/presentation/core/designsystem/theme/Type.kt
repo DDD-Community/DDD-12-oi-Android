4c50e87cd5b4aad62efec6881a8b7017f81732eb
kim dong kyung <kdkd422@naver.com>
2025-06-08 16:57:16 +0900

    style - Typography B,M,R 약어에서 Bold, Medium, Regular 수정

--- presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/theme/Type.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/theme/Type.kt
@@ -3,102 +3,99 @@ package com.ddd.oi.presentation.core.designsystem.theme
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.ddd.oi.presentation.R
 
 private val PretendardStyle = TextStyle(
     fontFamily = Pretendard,
 )
 
 internal val Typography = OiTypography(
-    headlineLargeB = PretendardStyle.copy(
+    headlineLargeBold = PretendardStyle.copy(
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp
     ),
-    headlineMediumB = PretendardStyle.copy(
+    headlineMediumBold = PretendardStyle.copy(
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp,
         lineHeight = 30.sp,
         letterSpacing = 0.sp
     ),
-    headlineSmallB = PretendardStyle.copy(
+    headlineSmallBold = PretendardStyle.copy(
         fontWeight = FontWeight.Bold,
         fontSize = 18.sp,
         lineHeight = 27.sp,
         letterSpacing = 0.sp
     ),
-    bodyLargeB = PretendardStyle.copy(
+    bodyLargeBold = PretendardStyle.copy(
         fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 20.8.sp,
         letterSpacing = 0.sp
     ),
-    bodyLargeM = PretendardStyle.copy(
+    bodyLargeMedium = PretendardStyle.copy(
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 20.8.sp,
         letterSpacing = 0.sp
     ),
-    bodyLargeR = PretendardStyle.copy(
+    bodyLargeRegular = PretendardStyle.copy(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 20.8.sp,
         letterSpacing = 0.sp
     ),
-    bodyMediumB = PretendardStyle.copy(
+    bodyMediumBold = PretendardStyle.copy(
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp,
         lineHeight = 18.2.sp,
         letterSpacing = 0.sp
     ),
-    bodyMediumM = PretendardStyle.copy(
+    bodyMediumMedium = PretendardStyle.copy(
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 18.2.sp,
         letterSpacing = 0.sp
     ),
-    bodyMediumR = PretendardStyle.copy(
+    bodyMediumRegular = PretendardStyle.copy(
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 18.2.sp,
         letterSpacing = 0.sp
     ),
-    bodySmallB = PretendardStyle.copy(
+    bodySmallBold = PretendardStyle.copy(
         fontWeight = FontWeight.Bold,
         fontSize = 12.sp,
         lineHeight = 15.6.sp,
         letterSpacing = 0.sp
     ),
-    bodySmallM = PretendardStyle.copy(
+    bodySmallMedium = PretendardStyle.copy(
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 15.6.sp,
         letterSpacing = 0.sp
     ),
-    bodySmallR = PretendardStyle.copy(
+    bodySmallRegular = PretendardStyle.copy(
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 15.6.sp,
         letterSpacing = 0.sp
     ),
-    bodyXSmallB = PretendardStyle.copy(
+    bodyXSmallBold = PretendardStyle.copy(
         fontWeight = FontWeight.Bold,
         fontSize = 11.sp,
         lineHeight = 14.3.sp,
         letterSpacing = 0.sp
     ),
-    bodyXSmallM = PretendardStyle.copy(
+    bodyXSmallMedium = PretendardStyle.copy(
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 14.3.sp,
         letterSpacing = 0.sp
     ),
-    bodyXSmallR = PretendardStyle.copy(
+    bodyXSmallRegular = PretendardStyle.copy(
         fontWeight = FontWeight.Normal,
         fontSize = 11.sp,
         lineHeight = 14.3.sp,
@@ -108,40 +105,40 @@ internal val Typography = OiTypography(
 
 @Immutable
 data class OiTypography(
-    val headlineLargeB: TextStyle,
-    val headlineMediumB: TextStyle,
-    val headlineSmallB: TextStyle,
+    val headlineLargeBold: TextStyle,
+    val headlineMediumBold: TextStyle,
+    val headlineSmallBold: TextStyle,
 
-    val bodyLargeB: TextStyle,
-    val bodyLargeM: TextStyle,
-    val bodyLargeR: TextStyle,
-    val bodyMediumB: TextStyle,
-    val bodyMediumM: TextStyle,
-    val bodyMediumR: TextStyle,
-    val bodySmallB: TextStyle,
-    val bodySmallM: TextStyle,
-    val bodySmallR: TextStyle,
-    val bodyXSmallB: TextStyle,
-    val bodyXSmallM: TextStyle,
-    val bodyXSmallR: TextStyle,
+    val bodyLargeBold: TextStyle,
+    val bodyLargeMedium: TextStyle,
+    val bodyLargeRegular: TextStyle,
+    val bodyMediumBold: TextStyle,
+    val bodyMediumMedium: TextStyle,
+    val bodyMediumRegular: TextStyle,
+    val bodySmallBold: TextStyle,
+    val bodySmallMedium: TextStyle,
+    val bodySmallRegular: TextStyle,
+    val bodyXSmallBold: TextStyle,
+    val bodyXSmallMedium: TextStyle,
+    val bodyXSmallRegular: TextStyle,
 )
 
 val LocalTypography = staticCompositionLocalOf {
     OiTypography(
-        headlineSmallB = PretendardStyle,
-        headlineMediumB = PretendardStyle,
-        headlineLargeB = PretendardStyle,
-        bodyLargeB = PretendardStyle,
-        bodyLargeM = PretendardStyle,
-        bodyLargeR = PretendardStyle,
-        bodyMediumB = PretendardStyle,
-        bodyMediumM = PretendardStyle,
-        bodyMediumR = PretendardStyle,
-        bodySmallB = PretendardStyle,
-        bodySmallM = PretendardStyle,
-        bodySmallR = PretendardStyle,
-        bodyXSmallB = PretendardStyle,
-        bodyXSmallM = PretendardStyle,
-        bodyXSmallR = PretendardStyle,
+        headlineSmallBold = PretendardStyle,
+        headlineMediumBold = PretendardStyle,
+        headlineLargeBold = PretendardStyle,
+        bodyLargeBold = PretendardStyle,
+        bodyLargeMedium = PretendardStyle,
+        bodyLargeRegular = PretendardStyle,
+        bodyMediumBold = PretendardStyle,
+        bodyMediumMedium = PretendardStyle,
+        bodyMediumRegular = PretendardStyle,
+        bodySmallBold = PretendardStyle,
+        bodySmallMedium = PretendardStyle,
+        bodySmallRegular = PretendardStyle,
+        bodyXSmallBold = PretendardStyle,
+        bodyXSmallMedium = PretendardStyle,
+        bodyXSmallRegular = PretendardStyle,
     )
 }
