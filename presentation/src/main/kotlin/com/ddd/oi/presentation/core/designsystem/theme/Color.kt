5913a392a8dda7a1fd2250e64db5289640476da7
kim dong kyung <kdkd422@naver.com>
2025-06-08 16:54:33 +0900

    style - 오타 수정 및 LocalColors 기본값으로 lightColorScheme로 설정

--- presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/theme/Color.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/theme/Color.kt
@@ -4,26 +4,18 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val TextPrimary = Color(0xFF171717)
 val TextSecondary = Color(0XFF262626)
 val TextTertiary = Color(0xFFA1A1A1)
 val TextDisabled = Color(0xFFD4D4D4)
-val TextonPrimary = Color(0xFFFFFFFF)
-val TextonSecondary = Color(0xFF039D58)
+val TextOnPrimary = Color(0xFFFFFFFF)
+val TextOnSecondary = Color(0xFF039D58)
 
 val BackgroundBrand = Color(0xFF039D58)
 val BackgroundBrandSecondary = Color(0xFFE5F8EE)
 val BackgroundPrimary = Color(0xFF171717)
 val BackgroundSecondary = Color(0xFFFAFAFA)
-val BackgroundTetiary = Color(0xFFE5E5E5)
+val BackgroundTertiary = Color(0xFFE5E5E5)
 
 val BorderPrimary = Color(0xFFE5E5E5)
 val BorderBrand = Color(0xFF039D58)
@@ -74,30 +66,5 @@ data class OiColors(
 )
 
 val LocalColors = staticCompositionLocalOf {
-    OiColors(
-        textPrimary = TextPrimary,
-        textSecondary = TextSecondary,
-        textTertiary = TextTertiary,
-        textDisabled = TextDisabled,
-        textonPrimary = TextonPrimary,
-        textonSecondary = TextonSecondary,
-        backgroundBrand = BackgroundBrand,
-        backgroundBrandSecondary = BackgroundBrandSecondary,
-        backgroundPrimary = BackgroundPrimary,
-        backgroundSecondary = BackgroundSecondary,
-        backgroundTetiary = BackgroundTetiary,
-        borderPrimary = BorderPrimary,
-        borderBrand = BorderBrand,
-        iconPrimary = IconPrimary,
-        iconSecondary = IconSecondary,
-        iconTetiary = IconTetiary,
-        iconDisabled = IconDisabled,
-        icononPrimary = IcononPrimary,
-        icononSecondary = IcononSecondary,
-        error = error,
-        caution = caution,
-        success = success,
-        info = info,
-        primary = primary
-    )
+    lightColorScheme
 }
