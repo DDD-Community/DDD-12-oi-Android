12c23b2a4f60e2dc9033752454afcc0843053ade
kim dong kyung <kdkd422@naver.com>
2025-06-10 09:28:07 +0900

    feat - (presentation)OiNavHost

---
+++ app/src/main/kotlin/com/ddd/oi/navigation/OiNavHost.kt
@@ -0,0 +1,30 @@
+package com.ddd.oi.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import com.ddd.oi.presentation.core.navigation.OiNavigator
+import com.ddd.oi.presentation.createschedule.createScheduleNavGraph
+import com.ddd.oi.presentation.home.homeNavGraph
+import com.ddd.oi.presentation.schedule.scheduleNavGraph
+import com.ddd.oi.presentation.scheduledetail.scheduleDetailNavGraph
+
+@Composable
+fun OiNavHost(
+    navigator: OiNavigator,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navigator.navController,
+        startDestination = navigator.startDestination,
+        modifier = modifier
+    ) {
+        homeNavGraph()
+        scheduleNavGraph()
+        createScheduleNavGraph(
+            navigateToScheduleDetail = navigator::navigateToScheduleDetail
+        )
+        scheduleDetailNavGraph()
+    }
+
+}
