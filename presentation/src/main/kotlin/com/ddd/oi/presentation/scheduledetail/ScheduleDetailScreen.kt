df21e6b81359dd834ef9e239a3fa8db594936180
kim dong kyung <kdkd422@naver.com>
2025-06-10 09:25:50 +0900

    feat - (presentation)일정 상세 스크린

---
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/scheduledetail/ScheduleDetailScreen.kt
@@ -0,0 +1,10 @@
+package com.ddd.oi.presentation.scheduledetail
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScheduleDetailScreen(modifier: Modifier = Modifier) {
+    Text(text = "일정 상세 화면")
+}
