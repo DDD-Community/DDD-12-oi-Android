80ee80d74deac2f04fc593431a12556b6fe4458a
kim dong kyung <kdkd422@naver.com>
2025-06-10 10:53:18 +0900

    feat - (presentation) 일정 화면

--- presentation/src/main/kotlin/com/ddd/oi/presentation/schedule/ScheduleScreen.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/schedule/ScheduleScreen.kt
@@ -1,13 +1,28 @@
 package com.ddd.oi.presentation.schedule
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.ddd.oi.presentation.core.designsystem.theme.OiTheme
 
 @Composable
 fun ScheduleScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text("일정 화면")
+    Column(modifier = modifier.fillMaxSize()) {
+        Column(modifier = modifier
+            .weight(1f)
+            .fillMaxSize()) {
+            Text("캘린더 영역")
+        }
+        Column(
+            modifier = modifier
+                .weight(1f)
+                .fillMaxSize()
+                .background(OiTheme.colors.backgroundTertiary)
+        ) {
+            Text("일정 리스트 영역")
+        }
     }
 }
