351dec75e4ee63f9a768e4cf0539fac53f585bf5
kim dong kyung <kdkd422@naver.com>
2025-06-10 10:51:52 +0900

    feat - (presentation)MainBottomBar

---
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/MainBottomBar.kt
@@ -0,0 +1,98 @@
+package com.ddd.oi.presentation.core.designsystem.component
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.ddd.oi.presentation.core.designsystem.theme.OiTheme
+import com.ddd.oi.presentation.core.designsystem.util.bottomBarShape
+import com.ddd.oi.presentation.core.navigation.MainTab
+import kotlinx.collections.immutable.PersistentList
+
+@Composable
+fun MainBottomBar(
+    modifier: Modifier = Modifier,
+    visible: Boolean,
+    tabs: PersistentList<MainTab>,
+    currentTab: MainTab?,
+    onTabSelected: (MainTab) -> Unit,
+) {
+    val menuBarShape = remember { bottomBarShape() }
+    AnimatedVisibility(
+        visible,
+        enter = fadeIn() + slideIn { IntOffset(0, it.height) },
+        exit = fadeOut() + slideOut { IntOffset(0, it.height) }
+    ) {
+        Surface(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            color = OiTheme.colors.backgroundTertiary,
+        ) {
+            Row(
+                modifier = modifier
+                    .fillMaxSize()
+                    .background(Color.White, menuBarShape),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                tabs.forEach { tab ->
+                    MainBottomBarItem(
+                        tab = tab,
+                        selected = tab == currentTab,
+                        onClick = { onTabSelected(tab) },
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun RowScope.MainBottomBarItem(
+    modifier: Modifier = Modifier,
+    tab: MainTab,
+    selected: Boolean,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .selectable(
+                selected = selected,
+                indication = null,
+                role = null,
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = onClick,
+            ),
+        contentAlignment = Alignment.Center,
+    ) {
+        Icon(
+            painter = painterResource(tab.iconResId),
+            contentDescription = stringResource(tab.contentDescription),
+            tint = if(selected) OiTheme.colors.iconPrimary else OiTheme.colors.iconTertiary,
+        )
+    }
+}
