f12efdf4fb266c3208037c6143d09f11be72c83a
kim dong kyung <kdkd422@naver.com>
2025-06-11 15:50:56 +0900

    feat - (presentation) Padding 값 정의한 Dimens

---
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/util/Dimens.kt
@@ -0,0 +1,24 @@
+package com.ddd.oi.presentation.core.designsystem.util
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val PaddingXSmall = 4.dp
+    val PaddingSmall = 8.dp
+    val PaddingMediumSmall = 12.dp
+    val PaddingMedium = 16.dp
+    val PaddingMediumLarge = 20.dp
+    val PaddingLargeSmall = 24.dp
+    val PaddingLargeMedium = 28.dp
+    val PaddingLarge = 32.dp
+    val BottomBarHeight = 40.dp
+    val FabSize = 48.dp
+    val FabOffset = (FabSize / 2) + PaddingMedium
+}
+
+object BottomBarShapeDimens {
+    val Width = 150.dp
+    val Height = 90.dp
+    val CurveStart = 75.dp
+    val CurveEnd = 85.dp
+}
