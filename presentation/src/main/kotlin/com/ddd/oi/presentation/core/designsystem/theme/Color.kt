24742cf374bcd498ff6350c4f517a4805035bf97
osemin <semin015123@gmail.com>
2025-06-12 23:25:38 +0900

    fix(Color) : add atomicColor and set semanticColor to atomic value

--- presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/theme/Color.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/theme/Color.kt
@@ -4,34 +4,42 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
+val asamo700 = Color(0xFF039D58)
+val asamo900 = Color(0xFF095E3A)
+
+val neutral100 = Color(0xFFF5F5F5)
+val neutral300 = Color(0xFFD4D4D4)
+
+val white = Color(0xFFFFFFFF)
+
 val TextPrimary = Color(0xFF171717)
 val TextSecondary = Color(0XFF262626)
 val TextTertiary = Color(0xFFA1A1A1)
 val TextDisabled = Color(0xFFD4D4D4)
-val TextOnPrimary = Color(0xFFFFFFFF)
-val TextOnSecondary = Color(0xFF039D58)
+val TextOnPrimary = white
+val TextOnSecondary = asamo700
 
-val BackgroundBrand = Color(0xFF039D58)
+val BackgroundBrand = asamo700
 val BackgroundBrandSecondary = Color(0xFFE5F8EE)
-val BackgroundPrimary = Color(0xFFFFFFFF)
+val BackgroundPrimary = white
 val BackgroundSecondary = Color(0xFFFAFAFA)
 val BackgroundTertiary = Color(0xFFE5E5E5)
 
 val BorderPrimary = Color(0xFFE5E5E5)
-val BorderBrand = Color(0xFF039D58)
+val BorderBrand = asamo700
 
 val IconPrimary = Color(0xFF171717)
 val IconSecondary = Color(0xFF404040)
 val IconTertiary = Color(0xFFA1A1A1)
 val IconDisabled = Color(0xFFD4D4D4)
-val IconOnPrimary = Color(0xFFFFFFFF)
-val IconOnSecondary = Color(0xFF039D58)
+val IconOnPrimary = white
+val IconOnSecondary = asamo700
 
 val error = Color(0xFFFB2C36)
 val caution = Color(0xFFF0B100)
 val success = Color(0xFF00C950)
 val info = Color(0xFF2B7FFF)
-val primary = Color(0xFF039D58)
+val primary = asamo700
 
 @Immutable
 data class OiColors(
