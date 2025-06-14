93073d371f0f32bd60f9e86eec9111df31a7fab2
kim dong kyung <kdkd422@naver.com>
2025-06-14 14:03:32 +0900

    style - (presentation) UiCategory 삭제 -> Category

--- presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/util/CategoryColor.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/util/CategoryColor.kt
@@ -4,16 +4,14 @@ import androidx.compose.ui.graphics.Color
 import com.ddd.oi.domain.model.Category
 import com.ddd.oi.presentation.core.designsystem.theme.indigo400
 import com.ddd.oi.presentation.core.designsystem.theme.lime400
-import com.ddd.oi.presentation.core.designsystem.theme.rose400
 import com.ddd.oi.presentation.core.designsystem.theme.teal400
 import com.ddd.oi.presentation.core.designsystem.theme.yellow400
-import com.ddd.oi.presentation.schedule.model.UiCategory
 
-fun UiCategory.getColor(): Color =
+fun Category.getColor(): Color =
     when (this) {
-        UiCategory.DAILY -> yellow400
-        UiCategory.DATE -> yellow400
-        UiCategory.TRAVEL -> teal400
-        UiCategory.BUSINESS -> indigo400
-        UiCategory.ETC -> lime400
+        Category.Daily -> yellow400
+        Category.Date -> yellow400
+        Category.Travel -> teal400
+        Category.Business -> indigo400
+        Category.Etc -> lime400
     }
