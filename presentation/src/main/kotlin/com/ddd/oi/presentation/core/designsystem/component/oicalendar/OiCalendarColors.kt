97d33d9c35a0199db8973aed1f8b6f3f5b1adbe0
kim dong kyung <kdkd422@naver.com>
2025-06-14 13:45:36 +0900

    add - (presentation) OiCalendarColors

--- presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/oicalendar/OiCalendarColors.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/oicalendar/OiCalendarColors.kt
@@ -9,40 +9,17 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.graphics.Color
 
 @Immutable
-class OiDatePickerColors
+class OiCalendarColors
 constructor(
-    private val containerColor: Color,
-    private val weekdayContentColor: Color,
-    val dayContentColor: Color,
-    private val disabledSelectedDayContentColor: Color,
+    val containerColor: Color,
+    val weekdayContentColor: Color,
+    private val dayContentColor: Color,
     private val disabledDayContentColor: Color,
     private val selectedDayContainerColor: Color,
     private val selectedDayContentColor: Color,
     private val todayContainerColor: Color,
     private val todayContentColor: Color,
 ) {
-    fun copy(
-        containerColor: Color = this.containerColor,
-        weekdayContentColor: Color = this.weekdayContentColor,
-        dayContentColor: Color = this.dayContentColor,
-        disabledSelectedDayContentColor: Color = this.disabledSelectedDayContentColor,
-        disabledDayContentColor: Color = this.disabledDayContentColor,
-        selectedDayContainerColor: Color = this.selectedDayContainerColor,
-        selectedDayContentColor: Color = this.selectedDayContentColor,
-        todayContainerColor: Color = this.todayContainerColor,
-        todayContentColor: Color = this.todayContentColor,
-    ) = OiDatePickerColors(
-        containerColor,
-        weekdayContentColor,
-        dayContentColor,
-        disabledSelectedDayContentColor,
-        disabledDayContentColor,
-        selectedDayContainerColor,
-        selectedDayContentColor,
-        todayContainerColor,
-        todayContentColor
-    )
-
     @Composable
     internal fun dayContentColor(
         isToday: Boolean,
@@ -52,7 +29,6 @@ constructor(
         val target =
             when {
                 selected && enabled -> selectedDayContentColor
-                selected && !enabled -> disabledSelectedDayContentColor
                 isToday -> todayContentColor
                 enabled -> dayContentColor
                 else -> disabledDayContentColor
@@ -64,14 +40,11 @@ constructor(
     internal fun dayContainerColor(
         today: Boolean,
         selected: Boolean,
-        enabled: Boolean,
         animate: Boolean
     ): State<Color> {
         val target = when {
-            today && selected -> selectedDayContainerColor
-            today && !selected -> Color.Gray
-            selected && enabled -> selectedDayContainerColor
-            selected && !enabled -> Color.Green
+            selected -> selectedDayContainerColor
+            today -> todayContainerColor
             else -> Color.Transparent
         }
         return if (animate) {
