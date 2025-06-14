26eee19856b9b4532c210489dc7872c119d417a2
kim dong kyung <kdkd422@naver.com>
2025-06-14 13:45:43 +0900

    add - (presentation) OiCalendarDefaults

--- presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/oicalendar/OiCalendarDefaults.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/oicalendar/OiCalendarDefaults.kt
@@ -1,35 +1,33 @@
 package com.ddd.oi.presentation.core.designsystem.component.oicalendar
 
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
 import com.ddd.oi.presentation.core.designsystem.theme.OiTheme
-import java.time.LocalDate
-import java.time.LocalTime
+import com.ddd.oi.presentation.core.designsystem.theme.white
 import java.time.ZoneId
 
-@ExperimentalMaterial3Api
 @Stable
-object OiDatePickerDefaults {
+object OiCalendarDefaults {
+
+    private const val ZONE_ID = "UTC"
+
+    val zone: ZoneId = ZoneId.of(ZONE_ID)
 
     @Composable
     fun colors(
-        containerColor: Color = Color.White,
+        containerColor: Color = white,
         weekdayContentColor: Color = OiTheme.colors.textPrimary,
         dayContentColor: Color = OiTheme.colors.textSecondary,
-        disabledSelectedDayContentColor: Color = OiTheme.colors.textDisabled,
         disabledDayContentColor: Color = OiTheme.colors.textDisabled,
-        selectedDayContainerColor: Color = OiTheme.colors.backgroundBrand,
-        selectedDayContentColor: Color = OiTheme.colors.textOnPrimary,
-        todayContainerColor: Color = OiTheme.colors.backgroundSecondary,
-        todayContentColor: Color = OiTheme.colors.textOnPrimary,
-    ): OiDatePickerColors = OiDatePickerColors(
+        selectedDayContainerColor: Color = OiTheme.colors.backgroundPrimary,
+        selectedDayContentColor: Color = white,
+        todayContainerColor: Color = OiTheme.colors.backgroundUnselected,
+        todayContentColor: Color = OiTheme.colors.textPrimary,
+    ): OiCalendarColors = OiCalendarColors(
         containerColor,
         weekdayContentColor,
         dayContentColor,
-        disabledSelectedDayContentColor,
         disabledDayContentColor,
         selectedDayContainerColor,
         selectedDayContentColor,
