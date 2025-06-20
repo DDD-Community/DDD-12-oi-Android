36e122ee0457fa457b60b6a026880fa88c4a342f
kim dong kyung <kdkd422@naver.com>
2025-06-20 15:43:39 +0900

    add - (data) ScheduleRemoteDataSource

---
+++ data/src/main/kotlin/com/ddd/oi/data/schedule/remote/ScheduleRemoteDataSource.kt
@@ -0,0 +1,10 @@
+package com.ddd.oi.data.schedule.remote
+
+import com.ddd.oi.data.schedule.model.ScheduleDto
+import com.ddd.oi.data.schedule.model.ScheduleRequestDto
+
+interface ScheduleRemoteDataSource {
+    suspend fun getScheduleList(year: Int, month: Int): Result<List<ScheduleDto>>
+
+    suspend fun uploadSchedule(schedule: ScheduleRequestDto): Result<ScheduleDto>
+}
