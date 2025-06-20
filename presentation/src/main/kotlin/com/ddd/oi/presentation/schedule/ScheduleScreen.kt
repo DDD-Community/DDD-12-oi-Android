b276e996f1149ba705a157998c388fb4cb3a98f0
kim dong kyung <kdkd422@naver.com>
2025-06-20 15:44:44 +0900

    feat - (presentation) 일정 스크린

--- presentation/src/main/kotlin/com/ddd/oi/presentation/schedule/ScheduleScreen.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/schedule/ScheduleScreen.kt
@@ -1,28 +1,214 @@
 package com.ddd.oi.presentation.schedule
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.ddd.oi.presentation.R
+import com.ddd.oi.presentation.core.designsystem.component.common.OiCard
+import com.ddd.oi.presentation.core.designsystem.component.common.OiChip
+import com.ddd.oi.presentation.core.designsystem.component.common.rippleOrFallbackImplementation
+import com.ddd.oi.presentation.core.designsystem.component.mapper.formatToScheduleHeaderDate
+import com.ddd.oi.presentation.core.designsystem.component.mapper.getCategoryIcon
+import com.ddd.oi.presentation.core.designsystem.component.mapper.getCategoryName
+import com.ddd.oi.presentation.core.designsystem.component.sechedule.MonthSelector
+import com.ddd.oi.presentation.core.designsystem.component.oicalendar.OiCalendar
+import com.ddd.oi.presentation.core.designsystem.component.oicalendar.getCurrentLocale
+import com.ddd.oi.presentation.core.designsystem.component.sechedule.OiLine
 import com.ddd.oi.presentation.core.designsystem.theme.OiTheme
+import com.ddd.oi.presentation.core.designsystem.theme.white
+import com.ddd.oi.presentation.schedule.contract.CategoryFilter
+import com.ddd.oi.presentation.schedule.contract.ScheduleState
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.datetime.LocalDate
+import org.orbitmvi.orbit.compose.collectAsState
 
 @Composable
-fun ScheduleScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.fillMaxSize()) {
-        Column(modifier = modifier
-            .weight(1f)
-            .fillMaxSize()) {
-            Text("캘린더 영역")
-        }
+fun ScheduleScreen(
+    modifier: Modifier = Modifier,
+    viewModel: ScheduleViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.collectAsState()
+    ScheduleScreen(
+        modifier = modifier,
+        onRefresh = viewModel::refresh,
+        scheduleState = uiState,
+        updateDate = viewModel::updateDate,
+        updateSelectedCategory = viewModel::updateSelectedCategory
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ScheduleScreen(
+    modifier: Modifier = Modifier,
+    onRefresh: () -> Unit,
+    scheduleState: ScheduleState,
+    updateDate: (LocalDate) -> Unit,
+    updateSelectedCategory: (CategoryFilter) -> Unit
+) {
+    val todaySchedule =
+        scheduleState.filteredSchedules[scheduleState.selectedDate] ?: persistentListOf()
+    PullToRefreshBox(
+        modifier = modifier,
+        onRefresh = onRefresh,
+        isRefreshing = scheduleState.isLoading,
+    ) {
         Column(
-            modifier = modifier
-                .weight(1f)
+            modifier = Modifier
                 .fillMaxSize()
-                .background(OiTheme.colors.backgroundContents)
+                .verticalScroll(rememberScrollState())
         ) {
-            Text("일정 리스트 영역")
+            MonthSelector(
+                selectedDate = scheduleState.selectedDate,
+                enabled = !scheduleState.isLoading,
+                onDateChange = updateDate
+            )
+            ScheduleCategoryFilter(
+                selectedCategory = scheduleState.selectedCategoryFilter,
+                categories = scheduleState.availableCategoryFilters,
+                updateSelectedCategory = updateSelectedCategory
+            )
+            OiCalendar(
+                selectedLocalDate = scheduleState.selectedDate,
+                onDateSelectionChange = updateDate,
+                schedules = scheduleState.calendarCategoryMap
+            )
+            ScheduleListHeader(selectedDate = scheduleState.selectedDate)
+            Column(
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                todaySchedule.forEach { schedule ->
+                    OiCard(
+                        schedule = schedule,
+                        onClick = { },
+                        onMoreClick = { }
+                    )
+                }
+            }
         }
     }
 }
+
+@Composable
+private fun ScheduleListHeader(
+    modifier: Modifier = Modifier,
+    selectedDate: LocalDate,
+) {
+    Row(
+        modifier = modifier
+            .padding(top = 24.dp)
+            .padding(horizontal = 16.dp)
+            .height(27.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        OiLine(modifier = Modifier.height(20.dp), color = OiTheme.colors.backgroundPrimary)
+        Text(
+            modifier = Modifier.padding(start = 12.dp),
+            text = formatToScheduleHeaderDate(
+                date = selectedDate,
+                locale = getCurrentLocale()
+            ),
+            style = OiTheme.typography.headlineSmallBold
+        )
+        Spacer(modifier = Modifier.weight(1f))
+
+        Text(
+            text = "일정 추가",
+            style = OiTheme.typography.bodySmallSemibold,
+            color = OiTheme.colors.textBrand
+        )
+
+    }
+}
+
+@Composable
+private fun ScheduleCategoryFilter(
+    modifier: Modifier = Modifier,
+    selectedCategory: CategoryFilter,
+    categories: ImmutableList<CategoryFilter>,
+    updateSelectedCategory: (CategoryFilter) -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .background(white)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .padding(vertical = 12.dp)
+                .horizontalScroll(rememberScrollState()),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            categories.forEach { category ->
+                val selected = category == selectedCategory
+                when (category) {
+                    CategoryFilter.All -> OiChip(
+                        modifier = Modifier
+                            .clickable(
+                                onClick = { updateSelectedCategory(category) },
+                                role = Role.Button,
+                                interactionSource = null,
+                                indication = rippleOrFallbackImplementation(bounded = false, radius = 12.dp)
+                            ),
+                        selected = selected,
+                        textStringRes = R.string.all
+                    )
+
+                    is CategoryFilter.Specific -> OiChip(
+                        modifier = Modifier.clickable(
+                            onClick = { updateSelectedCategory(category) },
+                            role = Role.Button,
+                            interactionSource = null,
+                            indication = rippleOrFallbackImplementation(bounded = false, radius = 12.dp)
+                        ),
+                        selected = selected,
+                        iconDrawableRes = category.category.getCategoryIcon(),
+                        textStringRes = category.category.getCategoryName()
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun ScheduleScreenPreview() {
+    OiTheme {
+        ScheduleScreen(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(OiTheme.colors.backgroundContents)
+        )
+    }
+}
+
