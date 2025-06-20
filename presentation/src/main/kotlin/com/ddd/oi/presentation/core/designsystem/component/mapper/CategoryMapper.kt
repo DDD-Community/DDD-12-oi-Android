4d2b2432baf3eeb4bf19e7d72e17b3e9b7ac29f3
kim dong kyung <kdkd422@naver.com>
2025-06-20 14:01:50 +0900

    feat - (presentation) 카테고리 아이콘 및 이름 변환 함수

--- presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/mapper/CategoryMapper.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/mapper/CategoryMapper.kt
@@ -2,6 +2,7 @@ package com.ddd.oi.presentation.core.designsystem.component.mapper
 
 import androidx.compose.ui.graphics.Color
 import com.ddd.oi.domain.model.Category
+import com.ddd.oi.presentation.R
 import com.ddd.oi.presentation.core.designsystem.theme.indigo400
 import com.ddd.oi.presentation.core.designsystem.theme.lime400
 import com.ddd.oi.presentation.core.designsystem.theme.teal400
@@ -13,4 +14,20 @@ internal fun Category.getColor(): Color = when (this) {
     Category.Travel -> teal400
     Category.Business -> indigo400
     Category.Etc -> lime400
+}
+
+internal fun Category.getCategoryIcon(): Int = when (this) {
+    Category.Daily -> R.drawable.ic_daily
+    Category.Travel -> R.drawable.ic_travel
+    Category.Date -> R.drawable.ic_date
+    Category.Business -> R.drawable.ic_business
+    Category.Etc -> R.drawable.ic_horizontalmore
+}
+
+internal fun Category.getCategoryName(): Int = when (this) {
+    Category.Daily -> R.string.daily
+    Category.Travel -> R.string.travel
+    Category.Date -> R.string.date
+    Category.Business -> R.string.business
+    Category.Etc -> R.string.etc
 }
