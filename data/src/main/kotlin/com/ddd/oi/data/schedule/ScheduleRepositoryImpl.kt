e26613df449ef0230eebfd550856b2cbaa9bf0cb
kim dong kyung <kdkd422@naver.com>
2025-06-20 15:43:58 +0900

    add - (data) ScheduleRepositoryImpl

---
+++ data/src/main/kotlin/com/ddd/oi/data/schedule/ScheduleRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.ddd.oi.data.schedule
+
+import com.ddd.oi.data.schedule.remote.ScheduleRemoteDataSource
+import com.ddd.oi.domain.model.schedule.Schedule
+import com.ddd.oi.domain.repository.ScheduleRepository
+import javax.inject.Inject
+
+class ScheduleRepositoryImpl @Inject constructor(
+    private val scheduleRemoteDataSource: ScheduleRemoteDataSource
+) : ScheduleRepository {
+    override suspend fun getScheduleList(year: Int, month: Int): Result<List<Schedule>> {
+        return scheduleRemoteDataSource.getScheduleList(year, month)
+            .mapCatching { dtoList ->
+                dtoList.map { it.toDomain() }
+            }
+    }
+}
