e90ca31b642a97c33273d75fc954cf02b54108cc
osemin <semin015123@gmail.com>
2025-06-22 00:40:22 +0900

    fix(UpsertScheduleState) : set uiState

--- presentation/src/main/kotlin/com/ddd/oi/presentation/upsertschedule/contract/UpsertScheduleState.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/upsertschedule/contract/UpsertScheduleState.kt
@@ -1,5 +1,30 @@
 package com.ddd.oi.presentation.upsertschedule.contract
 
+import androidx.compose.runtime.Stable
+import com.ddd.oi.domain.model.Category
+import com.ddd.oi.domain.model.Party
+import com.ddd.oi.domain.model.Transportation
+
+@Stable
 data class UpsertScheduleState(
-    val title: String = ""
-)
+    val id: Int,
+    val title: String,
+    val category: Category?,
+    val startDate: Long,
+    val endDate: Long,
+    val transportation: Transportation?,
+    val party: Set<Party>,
+) {
+
+    companion object {
+        val default = UpsertScheduleState(
+            id = -1,
+            title = "",
+            category = null,
+            startDate = System.currentTimeMillis(),
+            endDate = -1L,
+            transportation = null,
+            party = emptySet()
+        )
+    }
+}
