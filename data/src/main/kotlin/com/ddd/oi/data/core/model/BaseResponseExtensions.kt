ee17a68da7462bc22ee9146fbfc102b931579acc
kim dong kyung <kdkd422@naver.com>
2025-06-21 18:12:06 +0900

    feat - (data) safeApiCall 확장함수 작성

--- data/src/main/kotlin/com/ddd/oi/data/core/model/BaseResponseExtensions.kt
+++ data/src/main/kotlin/com/ddd/oi/data/core/model/BaseResponseExtensions.kt
@@ -6,10 +6,19 @@ internal fun <T> BaseResponse<T>.toResult(): Result<T> {
             Result.success(data)
         }
         statusCode in 200..299 && data == null -> {
-            Result.failure(Exception())
+            Result.failure(Exception("Data is null"))
         }
         else -> {
-            Result.failure(Exception())
+            Result.failure(Exception(error?.message ?: message))
         }
     }
+}
+
+internal suspend fun <T> safeApiCall(apiCall: suspend () -> BaseResponse<T>): Result<T> {
+    return runCatching {
+        apiCall()
+    }.fold(
+        onSuccess = { it.toResult() },
+        onFailure = { Result.failure(it) }
+    )
 }
