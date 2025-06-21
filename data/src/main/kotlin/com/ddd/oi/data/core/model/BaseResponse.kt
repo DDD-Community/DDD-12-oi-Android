3ac84a2ba6f300c46c6c4dc6abdc9693b114f242
kim dong kyung <kdkd422@naver.com>
2025-06-21 18:11:52 +0900

    refactor - (data) BaseResponse Error 추가

--- data/src/main/kotlin/com/ddd/oi/data/core/model/BaseResponse.kt
+++ data/src/main/kotlin/com/ddd/oi/data/core/model/BaseResponse.kt
@@ -6,9 +6,16 @@ import kotlinx.serialization.Serializable
 data class BaseResponse<T>(
     val statusCode: Int,
     val resultType: String,
-    val data: T?,
-    val message: String
+    val data: T? = null,
+    val message: String,
+    val error: ErrorResponse? = null
 )
 
 @Serializable
-data object EmptyData
+data class ErrorResponse(
+    val message: String,
+    val details: List<String> = emptyList()
+)
+
+@Serializable
+data object EmptyResponse
