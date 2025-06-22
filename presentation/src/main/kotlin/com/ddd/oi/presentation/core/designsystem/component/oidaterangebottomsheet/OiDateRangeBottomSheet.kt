6ac22de8095d6a9fb66ad1e102d489e5f6a338f4
kim dong kyung <kdkd422@naver.com>
2025-06-22 18:41:12 +0900

    add - (presentation) 데이터레인지 바텀시트

--- presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/oidaterangebottomsheet/OiDateRangeBottomSheet.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/oidaterangebottomsheet/OiDateRangeBottomSheet.kt
@@ -1,10 +1,24 @@
 package com.ddd.oi.presentation.core.designsystem.component.oidaterangebottomsheet
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import org.orbitmvi.orbit.compose.collectAsState
+
 
 @Composable
 fun OiDateRangeBottomSheet(
-
+    viewModel: DateRangeBottomSheetViewModel = hiltViewModel()
 ) {
+    var calendarMode by remember { mutableStateOf<CalendarMode>(CalendarMode.Range) }
+    val uiState by viewModel.collectAsState()
+}
 
+@Immutable
+private enum class CalendarMode {
+    Range, MonthGrid
 }
