b92d3c3f6fd56bfdf2b36aa7afa4fdaa633735b6
kim dong kyung <kdkd422@naver.com>
2025-06-22 14:41:37 +0900

    style - (presentation) 색상 변경

--- presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/mapper/CategoryMapper.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/mapper/CategoryMapper.kt
@@ -5,12 +5,13 @@ import com.ddd.oi.domain.model.schedule.Category
 import com.ddd.oi.presentation.R
 import com.ddd.oi.presentation.core.designsystem.theme.indigo400
 import com.ddd.oi.presentation.core.designsystem.theme.lime400
+import com.ddd.oi.presentation.core.designsystem.theme.rose400
 import com.ddd.oi.presentation.core.designsystem.theme.teal400
 import com.ddd.oi.presentation.core.designsystem.theme.yellow400
 
 internal fun Category.getColor(): Color = when (this) {
     Category.Daily -> yellow400
-    Category.Date -> yellow400
+    Category.Date -> rose400
     Category.Travel -> teal400
     Category.Business -> indigo400
     Category.Etc -> lime400
