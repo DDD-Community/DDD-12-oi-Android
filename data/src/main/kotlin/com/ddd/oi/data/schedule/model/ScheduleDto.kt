3d572d09cc0d53305e538f149228a6c47aac0ff4
kim dong kyung <kdkd422@naver.com>
2025-06-22 10:26:41 +0900

    feat - (data) toDomain 매퍼로 이동

--- data/src/main/kotlin/com/ddd/oi/data/schedule/model/ScheduleDto.kt
+++ data/src/main/kotlin/com/ddd/oi/data/schedule/model/ScheduleDto.kt
@@ -1,9 +1,7 @@
 package com.ddd.oi.data.schedule.model
 
-import com.ddd.oi.domain.model.schedule.Category
-import com.ddd.oi.domain.model.schedule.Party
+import com.ddd.oi.data.schedule.mapper.toDomain
 import com.ddd.oi.domain.model.schedule.Schedule
-import com.ddd.oi.domain.model.schedule.Transportation
 import kotlinx.datetime.LocalDate
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -26,20 +24,7 @@ data class ScheduleDto(
 
     @SerialName("groups")
     val groupList: List<GroupDto> = emptyList()
-) {
-    fun toDomain(): Schedule {
-        return Schedule(
-            id = scheduleId,
-            title = title,
-            category =  scheduleTag.toDomain(),
-            startedAt = LocalDate.parse(startDate),
-            endedAt = LocalDate.parse(endDate),
-            transportation = mobility.toDomain(),
-            partySet = groupList.map { it.toDomain() }.toSet(),
-            placeList = emptyList()
-        )
-    }
-}
+)
 
 
 @Serializable
@@ -58,15 +43,8 @@ enum class CategoryDto {
 
     @SerialName("OTHER")
     Etc;
+}
 
-}
-fun CategoryDto.toDomain(): Category = when (this) {
-    CategoryDto.Trip -> Category.Travel
-    CategoryDto.Date -> Category.Date
-    CategoryDto.Daily -> Category.Daily
-    CategoryDto.Business -> Category.Business
-    CategoryDto.Etc -> Category.Etc
-}
 
 @Serializable
 enum class TransportationDto {
@@ -82,12 +60,7 @@ enum class TransportationDto {
     @SerialName("WALK")
     Walk
 }
-fun TransportationDto.toDomain(): Transportation = when (this) {
-    TransportationDto.Car -> Transportation.Car
-    TransportationDto.Public -> Transportation.Public
-    TransportationDto.Bicycle -> Transportation.Bicycle
-    TransportationDto.Walk -> Transportation.Walk
-}
+
 
 @Serializable
 enum class GroupDto {
@@ -114,15 +87,4 @@ enum class GroupDto {
     
     @SerialName("OTHER")
     Other
-}
-
-fun GroupDto.toDomain(): Party = when (this) {
-    GroupDto.Solo -> Party.Alone
-    GroupDto.Couple -> Party.OtherHalf
-    GroupDto.Friend -> Party.Friend
-    GroupDto.Parents -> Party.Parent
-    GroupDto.Siblings -> Party.Sibling
-    GroupDto.Children -> Party.Children
-    GroupDto.Pet -> Party.Pet
-    GroupDto.Other -> Party.Etc
 }
