101c794b280ba5e84e45cf762a438fa060b67ad3
kim dong kyung <kdkd422@naver.com>
2025-06-23 10:11:22 +0900

    feat - enableEdgeToEdge 색상 white

--- app/src/main/kotlin/com/ddd/oi/MainActivity.kt
+++ app/src/main/kotlin/com/ddd/oi/MainActivity.kt
@@ -2,9 +2,12 @@ package com.ddd.oi
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.graphics.toArgb
 import com.ddd.oi.presentation.core.designsystem.theme.OiTheme
+import com.ddd.oi.presentation.core.designsystem.theme.white
 import com.ddd.oi.presentation.core.navigation.rememberOiNavigator
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,7 +15,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(white.toArgb(), white.toArgb()),
+            navigationBarStyle = SystemBarStyle.light(white.toArgb(), white.toArgb())
+        )
         setContent {
             OiTheme {
                 val navigator = rememberOiNavigator()
