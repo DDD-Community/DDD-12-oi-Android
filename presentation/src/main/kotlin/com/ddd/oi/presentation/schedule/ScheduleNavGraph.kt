3cd979706c6855e43dab318bd90e1f656b9124fa
kim dong kyung <kdkd422@naver.com>
2025-06-25 10:43:06 +0900

    refactor - navigateToCreateSchedule 인자 수정

--- presentation/src/main/kotlin/com/ddd/oi/presentation/schedule/ScheduleNavGraph.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/schedule/ScheduleNavGraph.kt
@@ -5,13 +5,15 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.ddd.oi.presentation.core.navigation.MainTabRoute
+import com.ddd.oi.presentation.core.navigation.Route
+import com.ddd.oi.presentation.schedule.model.ScheduleNavData
 
 fun NavController.navigateToSchedule(navOptions: NavOptions) {
     navigate(MainTabRoute.Schedule, navOptions)
 }
 
 fun NavGraphBuilder.scheduleNavGraph(
-    navigateToCreateSchedule: () -> Unit,
+    navigateToCreateSchedule: (ScheduleNavData?, Route.UpsertSchedule) -> Unit,
     onShowSnackbar: (String) -> Unit
 ) {
     composable<MainTabRoute.Schedule> { backStackEntry ->
