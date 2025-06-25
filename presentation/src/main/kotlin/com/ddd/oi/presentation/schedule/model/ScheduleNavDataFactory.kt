dd747533085f0b4d9fd064ac63f954556f5bad65
kim dong kyung <kdkd422@naver.com>
2025-06-25 10:42:46 +0900

    refactor - (presentation) ScheduleNavData 생성팩토리

---
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/schedule/model/ScheduleNavDataFactory.kt
@@ -0,0 +1,36 @@
+package com.ddd.oi.presentation.schedule.model
+
+import com.ddd.oi.domain.model.schedule.Schedule
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+
+object ScheduleNavDataFactory {
+    fun createLocalDateCreate(localDate: LocalDate): ScheduleNavData {
+        val date = localDate.atStartOfDayIn(TimeZone.currentSystemDefault()).toEpochMilliseconds()
+        return ScheduleNavData(startedAt = date)
+    }
+
+    fun createForCopy(original: Schedule): ScheduleNavData {
+        return ScheduleNavData(
+            title = original.title,
+            category = original.category,
+            transportation = original.transportation,
+            party = original.partySet,
+            placeList = original.placeList
+        )
+    }
+
+    fun createForEdit(original: Schedule): ScheduleNavData {
+        return ScheduleNavData(
+            id = original.id,
+            title = original.title,
+            category = original.category,
+            startedAt = original.startedAt.atStartOfDayIn(TimeZone.currentSystemDefault()).toEpochMilliseconds(),
+            endedAt = original.endedAt.atStartOfDayIn(TimeZone.currentSystemDefault()).toEpochMilliseconds(),
+            transportation = original.transportation,
+            party = original.partySet,
+            placeList = original.placeList
+        )
+    }
+}
