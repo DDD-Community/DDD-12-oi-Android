ad26a7df8de168cfaa094d9bbd25e640edb09bb9
kim dong kyung <kdkd422@naver.com>
2025-06-28 16:46:16 +0900

    style - (presentation) 바텀바 그림자 추가

--- presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/MainBottomBar.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/MainBottomBar.kt
@@ -11,18 +11,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 import com.ddd.oi.presentation.core.designsystem.theme.OiTheme
 import com.ddd.oi.presentation.core.designsystem.util.BottomBarDimens
 import com.ddd.oi.presentation.core.designsystem.util.Dimens
@@ -48,6 +47,10 @@ fun MainBottomBar(
             modifier = modifier
                 .fillMaxWidth()
                 .height(BottomBarDimens.bottomBarHeight)
+                .shadow(
+                    elevation = BottomBarDimens.bottomBarHeight / 2,
+                    shape = bottomBarShape(),
+                )
                 .background(Color.White, menuBarShape),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
