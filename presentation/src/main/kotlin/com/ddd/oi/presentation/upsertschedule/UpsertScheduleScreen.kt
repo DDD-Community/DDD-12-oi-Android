40cd78e0e32a2a3883dbcad2b361ea0cda1ea291
kim dong kyung <kdkd422@naver.com>
2025-06-28 22:54:20 +0900

    style - (presentation) 바텀시트 fillMaxHeight 조정

--- presentation/src/main/kotlin/com/ddd/oi/presentation/upsertschedule/UpsertScheduleScreen.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/upsertschedule/UpsertScheduleScreen.kt
@@ -7,9 +7,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -147,10 +147,7 @@ fun UpsertScheduleScreen(
         ) {
             Box(
                 modifier = Modifier
-                    .heightIn(
-                        min = 400.dp,
-                        max = 800.dp
-                    )
+                    .fillMaxHeight(0.75f)
                     .background(white)
             ) {
                 OiDateRangeBottomSheet { start, end, hasSchedules ->
