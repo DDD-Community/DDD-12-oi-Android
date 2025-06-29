0168382b87df56db1c391817f3e077b6ea77cd18
kim dong kyung <kdkd422@naver.com>
2025-06-29 21:07:51 +0900

    style - (presentation) 불필요한 import 제거

--- presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/oidaterangepicker/OiDateRangePicker.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/oidaterangepicker/OiDateRangePicker.kt
@@ -1,7 +1,6 @@
 package com.ddd.oi.presentation.core.designsystem.component.oidaterangepicker
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxWidth
