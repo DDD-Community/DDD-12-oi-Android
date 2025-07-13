f783235dca137e0d472a5914716519235aa19b97
kim dong kyung <kdkd422@naver.com>
2025-07-14 00:03:26 +0900

    style - 날짜 util 함수

--- presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/mapper/ScheduleStringFormatMapper.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/core/designsystem/component/mapper/ScheduleStringFormatMapper.kt
@@ -1,22 +1,33 @@
 package com.ddd.oi.presentation.core.designsystem.component.mapper
 
 import kotlinx.datetime.DayOfWeek
-import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
-import kotlinx.datetime.TimeZone
 import kotlinx.datetime.isoDayNumber
-import kotlinx.datetime.toLocalDateTime
 import java.time.format.TextStyle
 import java.util.Locale
 
 
-internal fun formatDateRange(startDate: LocalDate, endDate: LocalDate): String {
+internal fun formatDateRange(
+    startDate: LocalDate,
+    endDate: LocalDate,
+    isDetail: Boolean = false
+): String {
     val yearShort = startDate.year % 100
     val start = "%02d.%02d.%02d".format(yearShort, startDate.monthNumber, startDate.dayOfMonth)
-    if (startDate == endDate) return start
-    val end = "%02d.%02d".format(endDate.monthNumber, endDate.dayOfMonth)
+    if (startDate == endDate && !isDetail) return start
+    val end =
+        if (isDetail) "%02d.%02d.%02d".format(
+            yearShort,
+            endDate.monthNumber,
+            endDate.dayOfMonth
+        )
+        else "%02d.%02d".format(endDate.monthNumber, endDate.dayOfMonth)
 
     return "$start - $end"
+}
+
+internal fun formatToScheduleDetailActiveDate(activeDate: LocalDate): String {
+    return "%02d.%02d".format(activeDate.monthNumber, activeDate.dayOfMonth)
 }
 
 internal fun formatToScheduleHeaderDate(date: LocalDate, locale: Locale): String {
