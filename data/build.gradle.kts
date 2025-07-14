af6e8590d6c6e6c15be1e9489475d5bba7d87c0a
osemin <semin015123@gmail.com>
2025-07-14 22:58:26 +0900

    fix(build.gradle.kts) : add datastore

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.datastore.preferences)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
