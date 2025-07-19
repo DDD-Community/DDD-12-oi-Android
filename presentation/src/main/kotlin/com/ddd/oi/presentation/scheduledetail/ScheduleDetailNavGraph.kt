7b89bd4e219be8a5b9c038f96ce9a6a69c466195
kim dong kyung <kdkd422@naver.com>
2025-07-19 14:10:11 +0900

    feature - scheduleDetailNavGraph 커스텀 NavType

--- presentation/src/main/kotlin/com/ddd/oi/presentation/scheduledetail/ScheduleDetailNavGraph.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/scheduledetail/ScheduleDetailNavGraph.kt
@@ -3,14 +3,24 @@ package com.ddd.oi.presentation.scheduledetail
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import com.ddd.oi.domain.model.schedule.Schedule
 import com.ddd.oi.presentation.core.navigation.Route
+import com.ddd.oi.presentation.core.navigation.ScheduleNavType
+import kotlin.reflect.typeOf
 
-fun NavController.navigateToScheduleDetail(scheduleId: Long) {
-    navigate(Route.ScheduleDetail(scheduleId))
+fun NavController.navigateToScheduleDetail(schedule: Schedule) {
+    navigate(Route.ScheduleDetail(schedule))
 }
 
-fun NavGraphBuilder.scheduleDetailNavGraph() {
-    composable<Route.ScheduleDetail> {
-        ScheduleDetailScreen()
+fun NavGraphBuilder.scheduleDetailNavGraph(
+    onBackClick: () -> Unit
+) {
+    composable<Route.ScheduleDetail>(
+        typeMap = mapOf(typeOf<Schedule>() to ScheduleNavType)
+    ) {
+        ScheduleDetailScreen(
+            onBackClick = onBackClick
+        )
     }
 }
