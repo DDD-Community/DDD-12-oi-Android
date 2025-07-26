94119334fe68407d3590cb00832e2dc9a4910884
kim dong kyung <kdkd422@naver.com>
2025-07-26 13:21:17 +0900

    feat - updateScheduleDetail

--- data/src/main/kotlin/com/ddd/oi/data/scheduledetail/remote/ScheduleDetailRemoteSource.kt
+++ data/src/main/kotlin/com/ddd/oi/data/scheduledetail/remote/ScheduleDetailRemoteSource.kt
@@ -1,7 +1,11 @@
 package com.ddd.oi.data.scheduledetail.remote
 
+import com.ddd.oi.data.scheduledetail.model.EditPlaceDto
 import com.ddd.oi.data.scheduledetail.model.PlaceDto
+import com.ddd.oi.domain.model.schedule.SchedulePlace
 
 interface ScheduleDetailRemoteSource {
     suspend fun getScheduleDetails(scheduleId: Long): Result<List<PlaceDto>>
+
+    suspend fun updateScheduleDetail(scheduleId: Long, schedulePlace: SchedulePlace): Result<EditPlaceDto>
 }
