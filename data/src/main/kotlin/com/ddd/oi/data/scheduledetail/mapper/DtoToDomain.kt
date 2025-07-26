07beff7f593d872c971d9b754f5c34cc5247444c
kim dong kyung <kdkd422@naver.com>
2025-07-26 13:25:32 +0900

    feat PlaceInfo -> toDomain

--- data/src/main/kotlin/com/ddd/oi/data/scheduledetail/mapper/DtoToDomain.kt
+++ data/src/main/kotlin/com/ddd/oi/data/scheduledetail/mapper/DtoToDomain.kt
@@ -1,10 +1,10 @@
 package com.ddd.oi.data.scheduledetail.mapper
 
 import com.ddd.oi.data.scheduledetail.model.PlaceInfo
-import com.ddd.oi.domain.model.schedule.Place
+import com.ddd.oi.domain.model.schedule.SchedulePlace
 
-internal fun PlaceInfo.toDomain(): Place {
-    return Place(
+internal fun PlaceInfo.toDomain(): SchedulePlace {
+    return SchedulePlace(
         id = id,
         startTime = startTime,
         targetDate = targetDate,
