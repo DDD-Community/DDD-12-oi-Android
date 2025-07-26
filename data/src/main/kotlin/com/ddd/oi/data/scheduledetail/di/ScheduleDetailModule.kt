6cfaa7b13962ccc369c444f5e016ac14ff34dc60
kim dong kyung <kdkd422@naver.com>
2025-07-26 13:20:51 +0900

    feat - UpdateScheduleDetailUseCase

--- data/src/main/kotlin/com/ddd/oi/data/scheduledetail/di/ScheduleDetailModule.kt
+++ data/src/main/kotlin/com/ddd/oi/data/scheduledetail/di/ScheduleDetailModule.kt
@@ -10,6 +10,8 @@ import dagger.Binds
 import com.ddd.oi.data.scheduledetail.remote.ScheduleDetailApi
 import com.ddd.oi.data.scheduledetail.remote.ScheduleDetailRemoteDataSource
 import com.ddd.oi.data.scheduledetail.remote.ScheduleDetailRemoteDataSourceImpl
+import com.ddd.oi.domain.usecase.scheduledetail.UpdateScheduleDetailUseCase
+import com.ddd.oi.domain.usecase.scheduledetail.UpdateScheduleDetailUseCaseImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -40,6 +42,14 @@ object ScheduleDetailUseCaseModule {
         scheduleDetailRepository: ScheduleDetailRepository
     ): GetScheduleDetailsUseCase {
         return GetScheduleDetailsUseCaseImpl(scheduleDetailRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUpdateScheduleDetailUseCase(
+        scheduleDetailRepository: ScheduleDetailRepository
+    ): UpdateScheduleDetailUseCase {
+        return UpdateScheduleDetailUseCaseImpl(scheduleDetailRepository)
     }
 }
 
