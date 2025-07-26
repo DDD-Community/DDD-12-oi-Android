bdacbb5a7211817f319c4a2d05591433bbb80bb7
kim dong kyung <kdkd422@naver.com>
2025-07-26 13:22:20 +0900

    feat - updateSchedulePlaceTime

--- presentation/src/main/kotlin/com/ddd/oi/presentation/scheduledetail/ScheduleDetailViewModel.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/scheduledetail/ScheduleDetailViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.navigation.toRoute
 import com.ddd.oi.domain.model.schedule.Schedule
+import com.ddd.oi.domain.model.schedule.SchedulePlace
 import com.ddd.oi.domain.usecase.scheduledetail.GetScheduleDetailsUseCase
+import com.ddd.oi.domain.usecase.scheduledetail.UpdateScheduleDetailUseCase
 import com.ddd.oi.presentation.core.navigation.Route
 import com.ddd.oi.presentation.core.navigation.ScheduleNavType
 import com.ddd.oi.presentation.scheduledetail.contract.PlaceUiState
@@ -20,6 +22,7 @@ import kotlin.reflect.typeOf
 @HiltViewModel
 class ScheduleDetailViewModel @Inject constructor(
     private val getScheduleDetailsUseCase: GetScheduleDetailsUseCase,
+    private val updateScheduleDetailUseCase: UpdateScheduleDetailUseCase,
     private val savedStateHandle: SavedStateHandle,
 ) : ContainerHost<ScheduleDetailState, ScheduleDetailSideEffect>, ViewModel() {
 
@@ -30,23 +33,49 @@ class ScheduleDetailViewModel @Inject constructor(
     override val container =
         container<ScheduleDetailState, ScheduleDetailSideEffect>(ScheduleDetailState(schedule = schedule.schedule))
 
-    init {
-        getSchedulePlaces(schedule.schedule.id)
-    }
 
-    fun getSchedulePlaces(id: Long) = intent {
-        getScheduleDetailsUseCase(id)
-            .onSuccess { places->
-                Log.d("places", places.toString())
+
+    fun getSchedulePlaces() = intent {
+        getScheduleDetailsUseCase(schedule.schedule.id)
+            .onSuccess { places ->
+                Log.d("places sucess", places.toString())
                 reduce { state.copy(placeUiState = PlaceUiState.Success(places)) }
             }
             .onFailure {
-                Log.d("places", it.toString())
+                Log.d("places fail", it.toString())
                 reduce { state.copy(placeUiState = PlaceUiState.Error()) }
             }
     }
 
-    fun editPlaceTime() = intent {
+    fun updateSchedulePlaceTime(
+        selectedPlace: SchedulePlace,
+        startTime: String,
+    ) = intent {
+        val scheduleId = schedule.schedule.id
+        val place = selectedPlace.copy(startTime = startTime)
+        updateScheduleDetailUseCase(scheduleId = scheduleId, scheduleDetail = place)
+            .onSuccess { place ->
+                getSchedulePlaces()
+                Log.d("updateSchedulePlaceTime Success", place.toString())
+            }
+            .onFailure {
+                Log.d("updateSchedulePlaceTime Fail", it.toString())
+            }
+    }
 
+    fun updateSchedulePlaceMemo(
+        selectedPlace: SchedulePlace,
+        memo: String,
+    ) = intent {
+        val scheduleId = schedule.schedule.id
+        val place = selectedPlace.copy(memo = memo)
+        updateScheduleDetailUseCase(scheduleId = scheduleId, scheduleDetail = place)
+            .onSuccess { place ->
+                getSchedulePlaces()
+                Log.d("updateSchedulePlaceMemo Success", place.toString())
+            }
+            .onFailure {
+                Log.d("updateSchedulePlaceMemo Fail", it.toString())
+            }
     }
 }
