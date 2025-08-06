4f4d101266237508450c7e8ea349703d979fee51
osemin <semin015123@gmail.com>
2025-08-06 23:52:27 +0900

    fix(RecommendedDetailScreen) : add mock ui

--- presentation/src/main/kotlin/com/ddd/oi/presentation/recommendeddetail/RecommendedDetailScreen.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/recommendeddetail/RecommendedDetailScreen.kt
@@ -1,26 +1,44 @@
 package com.ddd.oi.presentation.recommendeddetail
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.ddd.oi.presentation.core.designsystem.component.common.OiHeader
+import com.ddd.oi.presentation.core.designsystem.component.common.OiSpotCard
 import com.ddd.oi.presentation.core.designsystem.theme.OiTheme
+import com.ddd.oi.presentation.core.designsystem.util.OiCardDimens
 
 @Composable
 fun RecommendedDetailScreen(
@@ -151,7 +169,93 @@ private fun RecommendedDetailContentTag(
 
 @Composable
 private fun RecommendedDetailPlaceContent() {
+    val items = listOf(
+        "Item 1",
+        "Item 2",
+        "Item 3",
+        "Item 4",
+        "Item 1",
+        "Item 2",
+        "Item 3",
+        "Item 4",
+        "Item 1",
+        "Item 2",
+        "Item 3",
+        "Item 4"
+    )
 
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(0.dp)
+    ) {
+        itemsIndexed(items) { index, item ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                if (index > 0) {
+                    Button(
+                        onClick = { },
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                            .width(48.dp)
+                            .height(28.dp)
+                            .align(Alignment.TopStart)
+                            .zIndex(1f)
+                            .offset(y = -14.dp)
+                    ) {
+                        Text("버튼 ${index + 1}")
+                    }
+                }
+
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color(0xFFFAFAFA))
+                        .padding(vertical = 8.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Row(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .width(48.dp)
+                                .fillMaxHeight(),
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .size(16.dp)
+                                    .align(Alignment.Center)
+                                    .background(
+                                        color = Color(0XFFA052F0),
+                                        shape = CircleShape
+                                    ),
+                            ) {
+                                Text(
+                                    modifier = Modifier.fillMaxSize(),
+                                    text = "${index.inc()}",
+                                    textAlign = TextAlign.Center,
+                                    style = OiTheme.typography.bodyXSmallSemibold,
+                                    color = Color.White
+                                )
+                            }
+                        }
+
+                        OiSpotCard(
+                            placeName = "경복궁",
+                            category = "궁궐",
+                            address = "서울 종로구 사직로 161",
+                            imageUrl = "https://picsum.photos/64/64"
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Composable
