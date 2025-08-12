bf898c9f05728561e1fde5499203ee489efa637e
kim dong kyung <kdkd422@naver.com>
2025-08-12 17:03:56 +0900

    feat - Retrofit DI Auth와 NonAuth 분리

--- data/src/main/kotlin/com/ddd/oi/data/core/retrofit/di/NetworkModule.kt
+++ data/src/main/kotlin/com/ddd/oi/data/core/retrofit/di/NetworkModule.kt
@@ -1,6 +1,7 @@
 package com.ddd.oi.data.core.retrofit.di
 
 import com.ddd.oi.data.BuildConfig
+import com.ddd.oi.data.auth.AuthService
 import com.ddd.oi.data.core.retrofit.api.ScheduleApiService
 import com.ddd.oi.data.place.remote.PlaceApi
 import com.ddd.oi.data.scheduledetail.remote.ScheduleDetailApi
@@ -16,6 +17,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import javax.inject.Qualifier
 import javax.inject.Singleton
+
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class NonAuthRetrofit
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -41,6 +47,20 @@ object NetworkModule {
         OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .build()
+
+    @Provides
+    @Singleton
+    @NonAuthRetrofit
+    fun provideNonAuthRetrofit(
+        json: Json,
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .client(okHttpClient)
+            .build()
+    }
 
     @Provides
     @Singleton
@@ -75,6 +95,14 @@ object NetworkModule {
         @OiApiRetrofit retrofit: Retrofit
     ): ScheduleDetailApi {
         return retrofit.create(ScheduleDetailApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthServiceApi(
+        @NonAuthRetrofit retrofit: Retrofit
+    ): AuthService {
+        return retrofit.create(AuthService::class.java)
     }
 }
 
