0ad86436451b701df65e418095b1bb2b5cd7251f
kim dong kyung <kdkd422@naver.com>
2025-08-13 01:58:14 +0900

    feat - 스낵바 및 navOption 추가

--- presentation/src/main/kotlin/com/ddd/oi/presentation/login/LoginNavGraph.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/login/LoginNavGraph.kt
@@ -6,16 +6,28 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.ddd.oi.presentation.core.designsystem.component.snackbar.OiSnackbarData
 import com.ddd.oi.presentation.core.navigation.Route
 
-fun NavController.navigateToLogin(navOptions: NavOptions) {
-    navigate(Route.Login)
+fun NavController.navigateToLogin() {
+    val navOption = NavOptions.Builder()
+        .setPopUpTo(
+            destinationId = this.graph.id,
+            inclusive = true,
+        )
+        .setLaunchSingleTop(true)
+        .build()
+    navigate(Route.Login, navOption)
 }
 
 fun NavGraphBuilder.loginNavGraph(
     navigateToHome: () -> Unit,
+    onShowSnackbar: (OiSnackbarData) -> Unit
 ) {
     composable<Route.Login> { backStackEntry ->
-        LoginScreen(modifier = Modifier.fillMaxSize())
+        LoginScreen(
+            modifier = Modifier.fillMaxSize(),
+            onShowSnackbar = onShowSnackbar
+        )
     }
 }
