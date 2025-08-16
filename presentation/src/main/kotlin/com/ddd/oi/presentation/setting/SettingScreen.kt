e8422e95474a4b3a4675fd605040c7d6f05b0fd8
osemin <semin015123@gmail.com>
2025-08-16 14:36:03 +0900

    fix(setting) : fix ui

--- presentation/src/main/kotlin/com/ddd/oi/presentation/setting/SettingScreen.kt
+++ presentation/src/main/kotlin/com/ddd/oi/presentation/setting/SettingScreen.kt
@@ -3,6 +3,7 @@ package com.ddd.oi.presentation.setting
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +12,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,6 +29,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ddd.oi.presentation.R
+import com.ddd.oi.presentation.core.designsystem.component.common.OiButton
+import com.ddd.oi.presentation.core.designsystem.component.common.OiButtonStyle
 import com.ddd.oi.presentation.core.designsystem.component.common.OiHeader
 import com.ddd.oi.presentation.core.designsystem.theme.OiTheme
 import com.ddd.oi.presentation.core.designsystem.theme.white
@@ -38,7 +45,7 @@ fun SettingScreen(
     viewModel: SettingViewModel = hiltViewModel()
 ) {
     val throttledNavigation = rememberThrottledNavigation()
-    
+
     SettingContent(
         onBack = { throttledNavigation(onBack) },
         onNavigateToProfile = { throttledNavigation(onNavigateToProfile) },
@@ -69,28 +76,42 @@ private fun SettingContent(
         }
     ) { padding ->
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding)
-                .padding(horizontal = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+            modifier = Modifier.padding(padding)
         ) {
-            Spacer(modifier = Modifier.height(16.dp))
-            
-            SettingMenuItem(
-                title = "프로필 관리",
-                onClick = onNavigateToProfile
-            )
-            
-            SettingMenuItem(
-                title = "공지사항",
-                onClick = onNavigateToAnnouncement
-            )
-            
-            SettingMenuItem(
-                title = "문의하기",
-                onClick = onNavigateToContactUs
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OiButton(
+                    modifier = Modifier,
+                    onClick = onNavigateToProfile,
+                    style = OiButtonStyle.Medium40Rect,
+                    title = "프로필 관리"
+                )
+            }
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth(),
+            ) {
+                itemsIndexed(
+                    listOf(
+                        "공지사항",
+                        "문의하기",
+                        "서비스 이용약관",
+                        "개인정보 처리 방침",
+                        "앱 버전"
+                    )
+                ) { index, item ->
+                    SettingMenuItem(
+                        title = item,
+                        onClick = onNavigateToAnnouncement
+                    )
+                }
+            }
         }
     }
 }
@@ -101,26 +122,36 @@ private fun SettingMenuItem(
     title: String,
     onClick: () -> Unit = {}
 ) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(56.dp)
-            .clickable { onClick() }
-            .padding(horizontal = 16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(
-            text = title,
-            style = OiTheme.typography.bodyLargeMedium,
-            color = OiTheme.colors.textPrimary
-        )
-        
-        Icon(
-            modifier = Modifier.size(24.dp),
-            painter = painterResource(R.drawable.ic_arrow_right),
-            contentDescription = null,
-            tint = Color.Unspecified
+    Box {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(56.dp)
+                .clickable { onClick() }
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = title,
+                style = OiTheme.typography.bodyLargeMedium,
+                color = OiTheme.colors.textPrimary
+            )
+
+            Icon(
+                modifier = Modifier.size(20.dp),
+                painter = painterResource(R.drawable.ic_chevron_right),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+        }
+
+        HorizontalDivider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            thickness = 1.dp,
+            color = OiTheme.colors.borderSecondary,
         )
     }
 }
